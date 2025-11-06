b76c37c2cb97dcf05ffe49bc65aafb0121033859
Leducanh2904 <ldanhsguk22@gmail.com>
2025-11-06 15:36:31 +0700

    add brcrypt to brcryptjs

--- server/jest/env.setup.js
+++ server/jest/env.setup.js
@@ -25,3 +25,5 @@ jest.mock('pino', () => {
     child: jest.fn(() => ({ info: jest.fn(), error: jest.fn(), warn: jest.fn(), debug: jest.fn() })),
   });
 });
+// Dùng bcryptjs thay cho bcrypt trong test để tránh lỗi ELF header
+jest.mock('bcrypt', () => require('bcryptjs'));

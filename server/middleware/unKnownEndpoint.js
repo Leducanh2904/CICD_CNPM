5f52d34ba5361f648ef8e925a45bfb9b59920e92
Leducanh2904 <ldanhsguk22@gmail.com>
2025-11-06 18:23:15 +0700

    fix error 200 api heath

--- server/middleware/unKnownEndpoint.js
+++ server/middleware/unKnownEndpoint.js
@@ -1,3 +1,5 @@
+app.get('/api/health', (req, res) => res.status(200).send('ok'));
+app.head('/api/health', (req, res) => res.sendStatus(200));
 const { ErrorHandler } = require("../helpers/error");
 
 // eslint-disable-next-line no-unused-vars
